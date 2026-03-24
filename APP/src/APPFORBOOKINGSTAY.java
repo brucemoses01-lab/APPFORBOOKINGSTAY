b073c0838d15dab93da25c17940b5f566a0b8e35
brucemoses01-lab <brucemoses01@gmail.com>
2026-03-24 14:40:40 +0530

    [bruemoses01-lab] Added [ feature/UC1-WelcomePage]

---
+++ APP/src/APPFORBOOKINGSTAY.java
@@ -0,0 +1,20 @@
+public class APPFORBOOKINGSTAY{
+    public static void main(String[] args) {
+
+        // Application Name and Version
+        String appName = "Book My Stay";
+        String version = "v1.0";
+
+        // Welcome Message
+        System.out.println("==================================================");
+        System.out.println(" Welcome to " + appName + " Application ");
+        System.out.println(" Version: " + version);
+        System.out.println("==================================================");
+
+        // System Status Message
+        System.out.println("System started successfully.");
+        System.out.println("Application is ready for further operations.");
+
+    }
+}
+
